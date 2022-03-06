7a3395e061beaa9edad0d7676c989f6763d795c5
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-03-06 23:46:43 +0800

    implement skeleton of app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
 
+export class AppSubmenuItem {
+  constructor(public title: string, public url: string) {}
+}
+
+export class AppMenuItem {
+  constructor(
+    public title: string,
+    public icon: string,
+    public submenuItemList: AppSubmenuItem[]
+  ) {}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'label.bkict.org';
+  public appTitle: string = 'Endoscopy Labeler';
+  public menuItemList: AppMenuItem[] = [
+    new AppMenuItem('Label data', 'edit', []),
+    new AppMenuItem('Settings', 'setting', [
+      new AppSubmenuItem('My profile', '/my-profile'),
+      new AppSubmenuItem('Manage users', '/manage-users'),
+      new AppSubmenuItem('Manage roles', '/manage-roles'),
+      new AppSubmenuItem('Manage permissions', '/manage-permissions'),
+      new AppSubmenuItem('Log out', '/logout'),
+    ]),
+    new AppMenuItem('Experimental features', 'experiment', []),
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
@@ -10,21 +9,29 @@ import en from '@angular/common/locales/en';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzBackTopModule } from 'ng-zorro-antd/back-top';
+import { EmptyStringOnMobileModule } from './pipes/empty-string-on-mobile/empty-string-on-mobile.module';
 
 registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    NzIconModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzBackTopModule,
+    EmptyStringOnMobileModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
