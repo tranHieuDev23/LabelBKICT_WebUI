af305ec33f92a4cf40f7f5e76f3f11c4c0bfe09a
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-03-09 22:21:36 +0800

    minor change in welcome page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  SessionManagementService,
+  SessionUserInfo,
+} from 'src/app/services/module/session-management';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.scss']
+  styleUrls: ['./welcome.component.scss'],
 })
 export class WelcomeComponent implements OnInit {
+  public sessionUserInfo: SessionUserInfo | null = null;
 
-  constructor() { }
+  constructor(
+    private readonly sessionManagementService: SessionManagementService,
+    private readonly router: Router
+  ) {}
 
   ngOnInit(): void {
+    (async () => {
+      this.sessionUserInfo =
+        await this.sessionManagementService.getUserFromSession();
+      if (this.sessionUserInfo === null) {
+        this.router.navigateByUrl('/login');
+        return;
+      }
+    })().then(
+      () => {},
+      (error) => {}
+    );
   }
-
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome.component';
 import { WelcomeRoutingModule } from './welcome-routing.module';
+import { NzGridModule } from 'ng-zorro-antd/grid';
 
 @NgModule({
   declarations: [WelcomeComponent],
-  imports: [CommonModule, WelcomeRoutingModule],
+  imports: [CommonModule, WelcomeRoutingModule, NzGridModule],
   exports: [WelcomeComponent],
 })
 export class WelcomeModule {}
