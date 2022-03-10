a74c4a16c51c866530b518885fe0b3c15f5b49b5
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-03-11 00:34:30 +0800

    implement welcome page

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserPermission } from 'src/app/services/dataaccess/api';
 import {
   SessionManagementService,
   SessionUserInfo,
 } from 'src/app/services/module/session-management';
+
+export class WelcomeSubmenuItem {
+  constructor(public title: string, public url: string) {}
+}
+
+export class WelcomeMenuItem {
+  constructor(
+    public title: string,
+    public icon: string,
+    public submenuItemList: WelcomeSubmenuItem[]
+  ) {}
+}
 
 @Component({
   selector: 'app-welcome',
@@ -12,6 +25,7 @@ import {
 })
 export class WelcomeComponent implements OnInit {
   public sessionUserInfo: SessionUserInfo | null = null;
+  public menuItemList: WelcomeMenuItem[] = [];
 
   constructor(
     private readonly sessionManagementService: SessionManagementService,
@@ -19,10 +33,64 @@ export class WelcomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.sessionUserInfo = this.sessionManagementService.getSessionUserInfo();
-    if (this.sessionUserInfo === null) {
+    const sessionUserInfo = this.sessionManagementService.getSessionUserInfo();
+    if (sessionUserInfo === null) {
       this.router.navigateByUrl('/login');
       return;
     }
+
+    this.sessionUserInfo = sessionUserInfo;
+    this.updateMenuItemList(sessionUserInfo);
+  }
+
+  private updateMenuItemList(sessionUserInfo: SessionUserInfo): void {
+    this.menuItemList = [];
+    const userPermissionNameSet = this.getUserPermissionNameSet(
+      sessionUserInfo.userPermissionList
+    );
+
+    // User management
+    if (this.isUserManagementMenuItemAvailable(userPermissionNameSet)) {
+      const submenuItemList: WelcomeSubmenuItem[] = [];
+      if (userPermissionNameSet.has('users.write')) {
+        submenuItemList.push(
+          new WelcomeSubmenuItem('Manage users', '/manage-users')
+        );
+      }
+      if (userPermissionNameSet.has('user_roles.write')) {
+        submenuItemList.push(
+          new WelcomeSubmenuItem('Manage user roles', '/manage-roles')
+        );
+      }
+      if (userPermissionNameSet.has('user_permissions.write')) {
+        submenuItemList.push(
+          new WelcomeSubmenuItem(
+            'Manage user permissions',
+            '/manage-permissions'
+          )
+        );
+      }
+      this.menuItemList.push(
+        new WelcomeMenuItem('User management', 'user', submenuItemList)
+      );
+    }
+  }
+
+  private getUserPermissionNameSet(
+    userPermissionList: UserPermission[]
+  ): Set<string> {
+    return new Set<string>(
+      userPermissionList.map((userPermission) => userPermission.permissionName)
+    );
+  }
+
+  private isUserManagementMenuItemAvailable(
+    userPermissionNameSet: Set<string>
+  ): boolean {
+    return (
+      userPermissionNameSet.has('users.write') ||
+      userPermissionNameSet.has('user_roles.write') ||
+      userPermissionNameSet.has('user_permissions.write')
+    );
   }
 }
--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 import { WelcomeComponent } from './welcome.component';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [WelcomeComponent],
-  imports: [CommonModule, WelcomeRoutingModule, NzGridModule],
+  imports: [
+    CommonModule,
+    WelcomeRoutingModule,
+    NzGridModule,
+    NzTypographyModule,
+    NzIconModule,
+    NzButtonModule,
+  ],
   exports: [WelcomeComponent],
 })
 export class WelcomeModule {}
