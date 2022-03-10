31c3928fcc525d7e65b990ca6ea8a56c3fd6022a
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-03-10 18:42:32 +0800

    implement /api/permissions API data access service

--- src/app/services/dataaccess/api/permissions.service.ts
+++ src/app/services/dataaccess/api/permissions.service.ts
@@ -1,7 +1,24 @@
+import { HttpStatusCode } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Axios } from 'axios';
+import axios, { Axios } from 'axios';
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+  UnknownAPIError,
+} from './errors';
 import { UserPermission } from './schemas';
 
+export class InvalidUserPermissionInformationError extends Error {
+  constructor() {
+    super('Invalid user role information');
+  }
+}
+
+export class UserPermissionNotFoundError extends Error {
+  constructor() {
+    super('Cannot find user permission');
+  }
+}
 @Injectable({
   providedIn: 'root',
 })
@@ -12,13 +29,48 @@ export class PermissionsService {
     permissionName: string,
     description: string
   ): Promise<UserPermission> {
-    throw new Error('not implemented');
+    try {
+      const response = await this.axios.post('/api/permissions', {
+        permission_name: permissionName,
+        description: description,
+      });
+      return UserPermission.fromJSON(response.data);
+    } catch (e) {
+      if (!axios.isAxiosError(e)) {
+        throw e;
+      }
+      switch (e.response?.status) {
+        case HttpStatusCode.BadRequest:
+          throw new InvalidUserPermissionInformationError();
+        case HttpStatusCode.Unauthorized:
+          throw new UnauthenticatedError();
+        case HttpStatusCode.Forbidden:
+          throw new UnauthorizedError();
+        default:
+          throw new UnknownAPIError(e);
+      }
+    }
   }
 
   public async getUserPermissionList(): Promise<{
     userPermissionList: UserPermission[];
   }> {
-    throw new Error('not implemented');
+    try {
+      const response = await this.axios.get('/api/permissions');
+      return response.data.user_permission_list.map(UserPermission.fromJSON);
+    } catch (e) {
+      if (!axios.isAxiosError(e)) {
+        throw e;
+      }
+      switch (e.response?.status) {
+        case HttpStatusCode.Unauthorized:
+          throw new UnauthenticatedError();
+        case HttpStatusCode.Forbidden:
+          throw new UnauthorizedError();
+        default:
+          throw new UnknownAPIError(e);
+      }
+    }
   }
 
   public async updateUserPermission(
@@ -26,10 +78,48 @@ export class PermissionsService {
     permissionName: string | undefined,
     description: string | undefined
   ): Promise<UserPermission> {
-    throw new Error('not implemented');
+    try {
+      const response = await this.axios.patch(`/api/permissions/${id}`, {
+        permission_name: permissionName,
+        description: description,
+      });
+      return UserPermission.fromJSON(response.data);
+    } catch (e) {
+      if (!axios.isAxiosError(e)) {
+        throw e;
+      }
+      switch (e.response?.status) {
+        case HttpStatusCode.BadRequest:
+          throw new InvalidUserPermissionInformationError();
+        case HttpStatusCode.Unauthorized:
+          throw new UnauthenticatedError();
+        case HttpStatusCode.Forbidden:
+          throw new UnauthorizedError();
+        case HttpStatusCode.NotFound:
+          throw new UserPermissionNotFoundError();
+        default:
+          throw new UnknownAPIError(e);
+      }
+    }
   }
 
   public async deleteUserPermission(id: number): Promise<void> {
-    throw new Error('not implemented');
+    try {
+      await this.axios.delete(`/api/permissions/${id}`);
+    } catch (e) {
+      if (!axios.isAxiosError(e)) {
+        throw e;
+      }
+      switch (e.response?.status) {
+        case HttpStatusCode.Unauthorized:
+          throw new UnauthenticatedError();
+        case HttpStatusCode.Forbidden:
+          throw new UnauthorizedError();
+        case HttpStatusCode.NotFound:
+          throw new UserPermissionNotFoundError();
+        default:
+          throw new UnknownAPIError(e);
+      }
+    }
   }
 }
