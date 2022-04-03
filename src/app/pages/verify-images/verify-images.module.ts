f7a98e6098bfd53a40bf014c8e17d1df995544ea
Tran Minh Hieu <tranhieu23.code@outlook.com>
2022-04-03 22:44:41 +0800

    removal of uncessnary module

--- src/app/pages/verify-images/verify-images.module.ts
+++ src/app/pages/verify-images/verify-images.module.ts
@@ -5,7 +5,6 @@ import { VerifyImagesRoutingModule } from './verify-images-routing.module';
 import { VerifyImagesComponent } from './verify-images.component';
 import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { ImageFilterOptionsSelectorModule } from 'src/app/components/image-filter-options-selector/image-filter-options-selector.module';
 import { ImageGridModule } from 'src/app/components/image-grid/image-grid.module';
@@ -20,7 +19,6 @@ import { ImageGridModule } from 'src/app/components/image-grid/image-grid.module
     ImageFilterOptionsSelectorModule,
     ImageGridModule,
     NzDropDownModule,
-    NzModalModule,
   ],
   exports: [VerifyImagesComponent],
 })
