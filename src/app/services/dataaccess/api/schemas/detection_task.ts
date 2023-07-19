8e0f51cac2c194bfdebf88754686614e22f19f5a
Trần Minh Hiếu <tranhieudev23@Trans-MacBook-Pro.local>
2023-07-19 16:13:13 +0700

    Simplify detection task data

--- src/app/services/dataaccess/api/schemas/detection_task.ts
+++ src/app/services/dataaccess/api/schemas/detection_task.ts
@@ -1,15 +1,21 @@
-import { Image } from './image';
-
 export enum DetectionTaskStatus {
   REQUESTED = 0,
   PROCESSING = 1,
   DONE = 2,
 }
 
+export class DetectionTaskImage {
+  constructor(public id: number, public thumbnailURL: string) {}
+
+  public static fromJSON(obj: any): DetectionTaskImage {
+    return new DetectionTaskImage(+obj.id, `${obj.thumbnail_url}`);
+  }
+}
+
 export class DetectionTask {
   constructor(
     public id: number,
-    public ofImage: Image,
+    public ofImage: DetectionTaskImage,
     public requestTime: number,
     public status: DetectionTaskStatus,
     public updateTime: number
@@ -18,7 +24,7 @@ export class DetectionTask {
   public static fromJSON(obj: any): DetectionTask {
     return new DetectionTask(
       +obj.id,
-      Image.fromJSON(obj.of_image),
+      DetectionTaskImage.fromJSON(obj.of_image),
       +obj.request_time,
       +obj.status as DetectionTaskStatus,
       +obj.update_time
